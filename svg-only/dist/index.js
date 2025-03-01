f32d931b61b6cf1b863a14170f15cda5d1ec87d7
shazee-04 <mgssrangajeewa@gmail.com>
2025-03-01 09:44:11 +0530

    updated snake colors

--- svg-only/dist/index.js
+++ svg-only/dist/index.js
@@ -27667,13 +27667,13 @@ const basePalettes = {
         colorDotBorder: "#1b1f230a",
         colorDots: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
         colorEmpty: "#ebedf0",
-        colorSnake: "purple",
+        colorSnake: "yellow",
     },
     "github-dark": {
         colorDotBorder: "#1b1f230a",
         colorEmpty: "#161b22",
         colorDots: ["#161b22", "#01311f", "#034525", "#0f6d31", "#00c647"],
-        colorSnake: "purple",
+        colorSnake: "yellow",
     },
 };
 // aliases
